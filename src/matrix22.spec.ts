7a40fac2a4c2287beeb1b290b402b3780b7b1d9f
Rui LOPES <rui.d.lopes@me.com>
2024-05-26 10:27:32 +0200

    refactor(matrix22): reorder tests

--- src/matrix22.spec.ts
+++ src/matrix22.spec.ts
@@ -8,12 +8,13 @@ describe('matrix22', () => {
         assert.deepStrictEqual(ZERO_MATRIX.squared(), ZERO_MATRIX);
     })
 
-    it('returns zero-matrix when any matrix is multiplied by zero-matrix', t => {
-        assert.deepStrictEqual(Q_MATRIX.times(ZERO_MATRIX), ZERO_MATRIX);
-    })
-
     it('squares Q matrix', t => {
         const qMatrixSquared: Matrix22 = new Matrix22(BigInt(2), BigInt(1), BigInt(1), BigInt(1));
         assert.deepStrictEqual(Q_MATRIX.squared(), qMatrixSquared);
     })
+
+    it('returns zero-matrix when any matrix is multiplied by zero-matrix', t => {
+        assert.deepStrictEqual(Q_MATRIX.times(ZERO_MATRIX), ZERO_MATRIX);
+    })
+
 })
