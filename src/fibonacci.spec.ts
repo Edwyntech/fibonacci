40ab2a026cb8e607f92d554e2796b17bf66afb35
Rui Lopes <rui.d.lopes@me.com>
2024-06-11 11:08:33 +0200

    feat(tests): add timeouts

--- src/fibonacci.spec.ts
+++ src/fibonacci.spec.ts
@@ -12,32 +12,32 @@ describe('fibonacci series', () => {
 
     const fibonacci: Fibonacci = new Fibonacci();
 
-    it('computes 0 for index 0', t => {
+    it('computes 0 for index 0', () => {
         assert.equal(fibonacci.of(0), 0);
     })
 
-    it('computes 1 for index 2', t => {
+    it('computes 1 for index 2', () => {
         assert.equal(fibonacci.of(2), 1);
     })
 
-    it('computes 4.3×10^208 for index 1 000', t => {
-        const _1_000 = BigInt(EXPECTED_FIBONACCI_VALUES._1_000);
-        assert.equal(fibonacci.of(1_000), _1_000);
+    it('computes 4.3×10^208 for index 1 000', {timeout: 1_000}, () => {
+        const FIBONACCI_OF_1_000 = BigInt(EXPECTED_FIBONACCI_VALUES._1_000);
+        assert.equal(fibonacci.of(1_000), FIBONACCI_OF_1_000);
     })
 
-    it('computes 3.4×10^2089 for index 10 000', t => {
-        const _10_000 = BigInt(EXPECTED_FIBONACCI_VALUES._10_000);
-        assert.equal(fibonacci.of(10_000), _10_000);
+    it('computes 3.4×10^2089 for index 10 000', {timeout: 1_000}, () => {
+        const FIBONACCI_OF_10_000 = BigInt(EXPECTED_FIBONACCI_VALUES._10_000);
+        assert.equal(fibonacci.of(10_000), FIBONACCI_OF_10_000);
     })
 
-    it('computes 2.6×10^20898 for index 100 000', t => {
-        const _100_000 = BigInt(EXPECTED_FIBONACCI_VALUES._100_000);
-        assert.equal(fibonacci.of(100_000), _100_000);
+    it('computes 2.6×10^20898 for index 100 000', {timeout: 1_000}, () => {
+        const FIBONACCI_OF_100_000 = BigInt(EXPECTED_FIBONACCI_VALUES._100_000);
+        assert.equal(fibonacci.of(100_000), FIBONACCI_OF_100_000);
     })
 
-    it('computes 1.9×10^208987 for index 1 000 000', t => {
-        const _1_000_000 = BigInt(EXPECTED_FIBONACCI_VALUES._1_000_000);
-        assert.equal(fibonacci.of(1_000_000), _1_000_000);
+    it('computes 1.9×10^208987 for index 1 000 000', {timeout: 1_000}, () => {
+        const FIBONACCI_OF_1_000_000 = BigInt(EXPECTED_FIBONACCI_VALUES._1_000_000);
+        assert.equal(fibonacci.of(1_000_000), FIBONACCI_OF_1_000_000);
     })
 
 })
